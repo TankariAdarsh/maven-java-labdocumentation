b531092f25a0d4f64da975df33a24d11225eac2d
nitin <nithinsrisai0730@gmail.com>
2023-12-25 18:06:57 +0530

    maven-java

---
+++ src/main/java/com/kmit/maven_java_lab/App.java
@@ -0,0 +1,14 @@
+package com.kmit.maven_java_lab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("lab documentation");
+    }
+}
